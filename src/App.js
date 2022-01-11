a57e36aa76c3acd34891e4b9142e97452c8aa067
mansi_829 <mansigundre1@gmail.com>
2022-01-11 12:28:53 +0530

    bootstrap-and-react-toastify-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
 import './App.css';
 
 const App = () =>
 {
   return (
     <div className="App">
+      <ToastContainer />
       Hello
     </div>
   );
