a57b9c907558fa1da92de42d880a5cac220c8167
mansi_829 <mansigundre1@gmail.com>
2022-01-11 12:50:27 +0530

    navbar-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import './App.css';
+import Navbar from './components/Navbar';
 
 const App = () =>
 {
   return (
     <div className="App">
       <ToastContainer />
+      <Navbar />
       Hello
     </div>
   );
